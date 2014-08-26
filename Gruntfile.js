4b926b48a5b72eee14804607e3467bf3914ca866
Dale Sande <dale.sande@gmail.com>
2014-08-26 12:02:51 -0700

    add in grunt watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,9 +6,19 @@ module.exports = function(grunt) {
           'stylesheets/app.css': 'sass/app.sass'
         }
       }
+    },
+    watch: {
+      source: {
+        files: ['sass/**/*.sass'],
+        tasks: ['sass'],
+        options: {
+          livereload: true
+        }
+      }
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-sass');
   grunt.registerTask('default', ['sass']);
 };
